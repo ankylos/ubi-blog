437f0256dd7cfa9948aa8093985fb92537518395
lambrosopos <lambrosopos@gmail.com>
2024-03-11 22:45:58 +0900

    feat: update browser implicit wait and send keys

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ load_dotenv()
 from config import read_config
 from plugins.tistory import TistoryBlog
 from utils.selenium_util import create_browser
+from interfaces import BlogTypes
 
 def main():
     """
@@ -16,15 +17,21 @@ def main():
     print(config_dict)
 
     browser = create_browser()
+    browser.implicitly_wait(20)
 
-    for blog_type in config_dict.get("blog", []):
-        if blog_type == "tistory":
+    for blog_type, blog_info in config_dict["blogs"].items():
+        if blog_type == BlogTypes.TISTORY.value:
             blog = TistoryBlog(
                 browser=browser,
+                blog_url=blog_info["blog_url"],
                 username=os.getenv("TISTORY_USERNAME", ""),
                 password=os.getenv("TISTORY_PASSWORD", "")
             )
+            print("logging in")
             blog.login()
+
+            print("Creating new post")
+            blog.new_post("hello")
 
 if __name__ == "__main__":
     main()
--- src/utils/selenium_util.py
+++ src/utils/selenium_util.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options 
 
@@ -9,3 +12,8 @@ def create_browser(headless=False):
     browser = Firefox(options=opts)
 
     return browser
+
+def send_keys_delayed(element, text):
+   for char in text:
+      element.send_keys(char)
+      time.sleep(random.uniform(0.03,0.2))
