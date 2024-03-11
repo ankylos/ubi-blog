9df37defe0fb0066adc9d39c0d8599ad621ef79f
lambrosopos <lambrosopos@gmail.com>
2024-03-11 22:45:39 +0900

    feat: change from yaml to toml

--- src/config.py
+++ src/config.py
@@ -1,17 +1,18 @@
 import sys
 from os.path import abspath, join
-from yaml import load, Loader
 
-DEFAULT_CONFIG_FILEPATH = abspath(join(__file__, "../..", "ubi-blog.config.yaml"))
+import tomli
+
+DEFAULT_CONFIG_FILEPATH = abspath(join(__file__, "../..", "ubi-blog.config.toml"))
 
 def read_config(config_filepath: str="") -> dict:
     if config_filepath == "":
         config_filepath = DEFAULT_CONFIG_FILEPATH
 
     try:
-        with open(config_filepath, "r") as f:
+        with open(config_filepath, mode="rb") as f:
             print(f"Loaded config file : {config_filepath}")
-            return load(f, Loader=Loader)
-    except Exception:
+            return tomli.load(f)
+    except Exception as exc:
         print(f"Unable to find config in : {config_filepath}")
-        sys.exit(1)
+        raise exc
